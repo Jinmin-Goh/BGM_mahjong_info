c1eac97dc86510d3da81975c3cd826fb0bf6c704
Jinmin Goh <eric970901@gmail.com>
2024-10-18 17:48:14 +0900

    Fix: fix timezone

--- src/app/mainDataTable.tsx
+++ src/app/mainDataTable.tsx
@@ -18,6 +18,7 @@ import {
   IconButton,
 } from '@mui/material';
 import { format } from 'date-fns';
+import moment from 'moment-timezone';
 import { DataGroup } from '@/types/data';
 import CloseIcon from '@mui/icons-material/Close';
 
@@ -189,9 +190,9 @@ export default function MainDataTable({ data }: MainDataTableProps) {
           <TableBody>
             {sortedData.length > 0 ? (
               sortedData.map((row) => (
-                <TableRow key={format(row.timestamp, 'yyyy년 MM월 dd일 HH시 mm분')}>
+                <TableRow key={moment.tz(row.timestamp, 'Asia/Seoul').format('YYYY년 MM월 DD일 HH시 mm분')}>
                   <TableCell component="th" scope="row">
-                    {format(row.timestamp, 'yyyy년 MM월 dd일 HH시 mm분')}
+                    {moment.tz(row.timestamp, 'Asia/Seoul').format('YYYY년 MM월 DD일 HH시 mm분')}
                   </TableCell>
                   <TableCell align="right">{row.firstPlaceName}</TableCell>
                   <TableCell align="right">{row.firstPlaceScore}</TableCell>
